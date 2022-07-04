f288cdff787aad5b3d1773ea8581067cceec9047
prabath1998 <prabhathudayanga1998@gmail.com>
2022-07-04 11:09:27 +0530

    Initial commit

---
+++ src/main/java/com/test/fileupdown/FileUpDownApplication.java
@@ -0,0 +1,13 @@
+package com.test.fileupdown;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileUpDownApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileUpDownApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/test/fileupdown/resource/FileResource.java
@@ -0,0 +1,9 @@
+package com.test.fileupdown.resource;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/file")
+public class FileResource {
+}
